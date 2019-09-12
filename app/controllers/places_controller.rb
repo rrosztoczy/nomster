2e9885bbb19d1085b6a11d6c2143070688f22e35
rrosztoczy <rrosztoczy@gmail.com>
2019-09-12 11:03:19 -0600

    fixed pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
   
     def index
-      @places = Place.all
+        @places = Place.order("name").page(params[:page])
     end
   
     def new
@@ -10,8 +10,8 @@ class PlacesController < ApplicationController
     end
   
     def create
-      Place.create(place_params)
-      redirect_to root_path
+        current_user.places.create(place_params)
+        redirect_to root_path
     end
   
     private
