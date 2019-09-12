2b33f7b55013bbc004cd065e82b8ab78ff372277
rrosztoczy <rrosztoczy@gmail.com>
2019-09-12 10:50:59 -0600

    added places table

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,22 @@
 class PlacesController < ApplicationController
-
-    def index 
-        @places = Place.paginate(page: params[:page], per_page: 10)
+    before_action :authenticate_user!, only: [:new, :create]
+  
+    def index
+      @places = Place.all
     end
-
+  
     def new
-        @place = Place.new
+      @place = Place.new
     end
-
+  
     def create
-        Place.create(place_params)
-        redirect_to root_path
+      Place.create(place_params)
+      redirect_to root_path
     end
-
+  
     private
-
+  
     def place_params
-    params.require(:place).permit(:name, :description, :address)
+      params.require(:place).permit(:name, :description, :address)
     end
-end
+  end
